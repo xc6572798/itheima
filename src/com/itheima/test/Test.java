54f52aff724ea18cec88dce6433247550725f7b8
root <18696385593@qq.com>
2018-11-21 16:37:55 +0800

    输出三次中文和英文的你好世界

---
+++ src/com/itheima/test/Test.java
@@ -0,0 +1,12 @@
+package itheima.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好世界!");
+        System.out.println("helloworld!");
+        System.out.println("你好世界!");
+        System.out.println("helloworld!");
+        System.out.println("你好世界!");
+        System.out.println("helloworld!");
+    }
+}
